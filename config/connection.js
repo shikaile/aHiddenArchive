506f48cb1e734244fade4cddb5be5697735d6488
史开乐 <csurfssecure@gmail.com>
2023-02-10 12:29:57 -0500

    serving the server

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else{
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        //possible error throw here
+        host: (process.env.HOST),
+        dialect: 'mysql',
+        port: 3306
+    });
+};
+module.exports = sequelize
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS a_hidden_archive;
+
+CREATE DATABASE a_hidden_archive;
